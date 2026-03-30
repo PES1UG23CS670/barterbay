985171f50b7a17a0784a3dbffd4baef1d10e8fdc
Divya <divyathumma9731@gmail.com>
2026-03-30 15:20:14 +0530

    Merged admin feature and updated frontend API integration

--- barterbay-frontend/src/main/java/com/barterbay/frontend/service/ApiService.java
+++ barterbay-frontend/src/main/java/com/barterbay/frontend/service/ApiService.java
@@ -1,14 +1,16 @@
 package com.barterbay.frontend.service;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 
 public class ApiService {
 
-    public boolean signup(String username, String password) throws Exception {
+    public boolean signup(String username, String password) throws IOException {
 
-        URL url = new URL("http://localhost:8080/api/users/signup?username="
-                + username + "&password=" + password);
+        URL url = URI.create("http://localhost:8080/api/users/signup?username="
+            + username + "&password=" + password).toURL();
 
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("POST");
